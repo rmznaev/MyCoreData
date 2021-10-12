9d6ce90fdaf8b7c3c1d1dd1180fb22069775351f
Ramazan Abdullayev <ramazan.abdullayev@accessbank.az>
2021-10-12 11:40:23 +0400

    Initial Commit

---
+++ MyCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCoreData
+//
+//  Created by Ramazan Abdullayev on 12.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
